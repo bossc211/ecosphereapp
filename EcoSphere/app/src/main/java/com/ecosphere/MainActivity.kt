4d9745c3f49721f35e78c9bd39122f94570eb66d
bossc211 <161426496+bossc211@users.noreply.github.com>
2025-10-26 02:40:36 +0530

    Add import for currentBackStackEntryAsState

--- EcoSphere/app/src/main/java/com/ecosphere/MainActivity.kt
+++ EcoSphere/app/src/main/java/com/ecosphere/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ecosphere
 
+import androidx.navigation.compose.currentBackStackEntryAsState
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
