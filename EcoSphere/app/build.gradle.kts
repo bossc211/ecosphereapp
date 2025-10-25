68c80460fd9fc8c8fe768cd936be18163fb63c5f
bossc211 <161426496+bossc211@users.noreply.github.com>
2025-10-26 00:48:45 +0530

    Add Material Design library dependency

--- EcoSphere/app/build.gradle.kts
+++ EcoSphere/app/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
