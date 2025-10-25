12a4f9effa6e9afcc9db9aa80ac75d272a2d9a45
bossc211 <161426496+bossc211@users.noreply.github.com>
2025-10-26 00:30:07 +0530

    Update settings.gradle.kts for plugin management

--- EcoSphere/settings.gradle.kts
+++ EcoSphere/settings.gradle.kts
@@ -1,2 +1,17 @@
+// settings.gradle.kts  (place at your project root or inside EcoSphere/ if you use a subfolder)
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 rootProject.name = "EcoSphere"
 include(":app")
